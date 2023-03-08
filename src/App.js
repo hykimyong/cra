db067e9240b1fcbb8c83ce18c1963f8c64d60d72
hykimyong <hykimyong@naver.com>
2023-03-08 22:50:24 +0900

    useState
    컴포넌트 내의 상태 -> 자신의 출력값을 변경
    Class component -> State LifeCycle
    Functional component -> 훅으로관리
    유의사항 ->직접수정하면안되고 비동기적일수있다

--- src/App.js
+++ src/App.js
@@ -2,10 +2,14 @@ import logo from './logo.svg';
 import './App.css';
 import Composition from './components/2-4.Props/Composition';
 import Extraction from './components/2-4.Props/Extraction/Extraction';
+import ClassComponent from './components/2-5.State/ClassComponent';
+import FunctionalComponent from './components/2-5.State/FunctionalComponent';
 
 function App() {
   return (
     <>
+    <FunctionalComponent/>
+    <ClassComponent/>
     <Composition/>
     <Extraction/>
     </>
