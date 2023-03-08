2a1fdb0bcfc964e3fa01e1dabf00718faf94002d
hykimyong <hykimyong@naver.com>
2023-03-08 23:08:18 +0900

    lifeCycle
    constructor-> state 초기화 및 메서드 바인딩
    componentDidmount -> dom 노드 초기화 및 데이터 fetch
    componentWillunmount -> 타이머 제거 및 요청 취소 및 구독 해제
    Functional Component -> hook으로 대부분 구현가능
    arrow function으로 쓰면 바깥과의 context를 공유해서 bind해주지않아도 알수있다

--- src/App.js
+++ src/App.js
@@ -3,15 +3,14 @@ import './App.css';
 import Composition from './components/2-4.Props/Composition';
 import Extraction from './components/2-4.Props/Extraction/Extraction';
 import ClassComponent from './components/2-5.State/ClassComponent';
+import ClassComponent2 from './components/2-6.LifeCycle/ClassComponent';
 import FunctionalComponent from './components/2-5.State/FunctionalComponent';
 
 function App() {
   return (
     <>
-    <FunctionalComponent/>
-    <ClassComponent/>
-    <Composition/>
-    <Extraction/>
+    <ClassComponent2/>
+    
     </>
   );
 }
---
+++ src/components/2-6.LifeCycle/ClassComponent.jsx
@@ -0,0 +1,40 @@
+import React, { Component } from 'react'
+
+export default class ClassComponent extends Component {
+    constructor(props){
+        super(props);
+        console.log('constructor');
+        this.state = {date:new Date()};
+        // this.handleClick = this.handleClick.bind(this);
+    }
+    componentDidMount(){
+        console.log('componentDidMount');
+        this.timerID = setInterval(()=>this.tick(),10000);
+    }
+    componentDidUpdate(){
+        console.log('componentDidUpdate');
+    }
+    componentWillUnmount(){
+        console.log('componentWillUnmount');
+        clearInterval((this.timerID));
+    }
+
+    tick(){
+        console.log('consttickructor');
+        this.setState({date:new Date()});
+    }
+
+    handleClick = () => {
+        alert(this.state.date);
+    }
+
+  render() {
+    console.log('render');
+    return (
+      <div>
+        <h1 onClick={this.handleClick}>Hello, world!1</h1>
+        <h1>{this.state.date.toLocaleTimeString()}</h1>
+      </div>
+    )
+  }
+}
