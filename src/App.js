0bc37c5aae107157f0221a5498888df21b61f8f4
hykimyong <hykimyong@naver.com>
2023-04-07 23:39:10 +0900

    불필요 소스 제거

--- src/App.js
+++ src/App.js
@@ -1,28 +1,6 @@
 // import logo from './logo.svg';
 import './App.css';
-import Counter2 from './components/Counter2';
-// import DateFnsExample from './components/DateFns/DateFnsExample';
-// import DayjsExample from './components/Dayjs/DayjsExample';
-// import MomentExample from './components/MomentExample/MomentExample';
 import Counter from './features/counter/Counter';
-// import Example from './components/3-11.Portal/Example';
-// import Component from './components/3-14.PropTypes/Component';
-// import ControlledComponent from './components/2-10.Form/ControlledComponent';
-// import UncontrolledComponent from './components/2-10.Form/UncontrolledComponent';
-// import State from './components/3-3.Hooks/State';
-// import Reducer from './components/3-3.Hooks/Reducer';
-// import Memo from './components/3-8.Memoization/Memo';
-// import EmotionExample from './components/EmotionExample/EmotionExample';
-// import Example from './components/SassExample/Example';
-// import StyledComponentExample from './components/StyledComponentsExample/StyledComponentExample';
-// import List from './components/2-9.List/List';
-// import Event from './components/2-7.Event/Event';
-// import Condition from './components/2-8.ConditionalRendering/Condition';
-// import Composition from './components/2-4.Props/Composition';
-// import Extraction from './components/2-4.Props/Extraction/Extraction';
-// import ClassComponent from './components/2-5.State/ClassComponent';
-// import ClassComponent2 from './components/2-6.LifeCycle/ClassComponent';
-// import FunctionalComponent from './components/2-5.State/FunctionalComponent';
 
 function App() {
   return (
