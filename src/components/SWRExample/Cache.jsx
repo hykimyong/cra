2213745a9c68661aec015712f54886a16fd3b871
hykimyong <hykimyong@naver.com>
2023-05-18 21:49:25 +0900

    mutate 테스트

--- src/components/SWRExample/Cache.jsx
+++ src/components/SWRExample/Cache.jsx
@@ -3,18 +3,18 @@ import { SWRConfig, useSWRConfig } from 'swr'
 
 export default function Cache() {
   return (
-    <SWRConfig>
+    <SWRConfig value={{refreshInterval : 1000}}>
         <Page/>
     </SWRConfig>
   )
 }
 
 const Page = ()=>{
-    const {cache} = useSWRConfig();
+    const {cache, mutate} = useSWRConfig();
     return(
         <div>
             <button onClick={()=>{
-                console.log(`check:${JSON.stringify(cache.get("/api/user/1212"))}`);
+                mutate("/api/user/1212");
             }}>
                 check
             </button>
